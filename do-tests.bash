d0140350da96aef9b0508e3fa921bc7d6b3e3353
SAI TEJA GILUKARA <78465749+saiteja12-g@users.noreply.github.com>
2023-12-16 14:26:02 -0500

    setup test_coverage

--- do-tests.bash
+++ do-tests.bash
@@ -31,37 +31,9 @@ colcon test
 colcon test-result --test-result-base build/RadRoverRescue
 
 ##############################
-# 4. generate individual coverage reports:
+# 4. show the combined coverage report
 ##############################
-## 4.1 RadRoverRescue:
-colcon build \
-       --event-handlers console_cohesion+ \
-       --packages-select RadRoverRescue \
-       --cmake-target "test_coverage" \
-       --cmake-arg -DUNIT_TEST_ALREADY_RAN=1
-RadRoverRescue_COVERAGE_INFO=./build/RadRoverRescue/test_coverage.info
-## 4.2 RadRoverRescue:
 ros2 run RadRoverRescue generate_coverage_report.bash
-RadRoverRescue_COVERAGE_INFO=./build/RadRoverRescue/test_coverage.info
+open build/RadRoverRescue/test_coverage/index.html
 
-##############################
-# 5. Combine coverage reports
-##############################
-## create output directory
-COMBINED_TEST_COVERAGE=combined_test_coverage
-if [[ -d $COMBINED_TEST_COVERAGE ]]; then
-   rm -rf $COMBINED_TEST_COVERAGE
-fi
-mkdir $COMBINED_TEST_COVERAGE
-## combine the reports
-ALL_COVERAGE_INFO=./build/test_coverage_merged.info
-lcov -a $RadRoverRescue_COVERAGE_INFO -a \
-     $RadRoverRescue_COVERAGE_INFO -o \
-     $ALL_COVERAGE_INFO
-
-genhtml --output-dir $COMBINED_TEST_COVERAGE $ALL_COVERAGE_INFO
-
-##############################
-# 6. show the combined coverage report
-##############################
-open $COMBINED_TEST_COVERAGE/index.html || true
+# open $test_coverage/index.html || true
