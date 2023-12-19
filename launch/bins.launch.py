0e5322ebc40b74b71a76db961ddf5fcae427bc1a
SAI TEJA GILUKARA <78465749+saiteja12-g@users.noreply.github.com>
2023-12-18 22:25:36 -0500

    adding launch files

---
+++ launch/bins.launch.py
@@ -0,0 +1,45 @@
+import os
+import random
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_random_bins(num_bins):
+
+    x_bins = random.sample(range(0, 2), num_bins)
+    y_bins = random.sample(range(1, 7), num_bins)
+    
+    return x_bins, y_bins
+
+def gen_node(idx, xpos, ypos):
+    model = os.path.join(get_package_share_directory('RadRoverRescue'),
+                         'models')
+    
+    return Node(
+    package='gazebo_ros', 
+    executable='spawn_entity.py',
+    arguments=['-entity', 'trash_bin{}'.format(idx), 
+            '-file', model + '/bin_cylinder/model.sdf',
+            '-x', str(xpos),
+            '-y', str(ypos),
+            '-z', '0.0',
+            '-Y', '0.0'],
+            output='screen')
+
+def generate_launch_description():
+    
+    num_bins = 1
+    x_bins, y_bins = generate_random_bins(num_bins)
+    nodes = []
+    for i in range(num_bins):
+        nodes.append(gen_node(i+1, 0, 1))
+        # nodes.append(gen_node(i+1, 1.5, 0.5))
+
+    model = os.path.join(get_package_share_directory('RadRoverRescue'),
+                         'models')
+
+    return LaunchDescription([
+        nodes[0],
+    ])
+        
